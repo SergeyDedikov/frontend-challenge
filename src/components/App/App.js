deb5242e5a06469cb780348aa539f4f0afe2f0d5
SergeyDedikov <serded59@yandex.ru>
2022-05-29 17:12:39 +0500

    feat: components and routes structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,34 @@
 import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import api from "../../utils/api";
 
 export default function App() {
-  return onLoad ? <></> : <p>Загрузка...</p>;
+  const [onLoad, setOnLoad] = useState(false);
+  const [cats, setCats] = useState([]);
+  const [favoriteCats, setFavoriteCats] = useState([]);
+
+  useEffect(() => {
+    setOnLoad(true);
+  }, []);
+
+  return onLoad ? (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<CatsList cats={cats} />} />
+          <Route
+            path="/favorite-cats"
+            element={<CatsList favoriteCats={favoriteCats} />}
+          />
+        </Routes>
+      </main>
+    </>
+  ) : (
+    <p>Загрузка...</p>
+  );
 }
