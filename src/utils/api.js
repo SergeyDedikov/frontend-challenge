08adbeb0c5261812aa03323dbaa854c102f273df
SergeyDedikov <serded59@yandex.ru>
2022-05-30 18:25:01 +0500

    feat: setting params API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const BASE_URL_API =
-  "https://api.thecatapi.com/v1/images/search?limit=15&size=full";
+  "https://api.thecatapi.com/v1/images/search?limit=100&size=small";
 const API_KEY = "6ce97757-eab3-47e6-b462-e1f680e85366";
 
 class Api {
